0b2da09aef8c28e7f5b8a0dd4e7b1cc31d6ad793
Finn Casey Fierro <73711482+FinnCastro@users.noreply.github.com>
2023-03-05 20:03:46 +0000

    Mew

--- PythonDB/InitialIdentifier.py
+++ PythonDB/InitialIdentifier.py
@@ -5,14 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import re #Using RegEx to filter through the symbols. 
+from multiprocessing import Process, Queue
 
 # Binance library and client (no API key required).
 from binance import Client
 client = Client()
 
-# This app gets a few months worth of minute data for each coin, and identifies if there was any pumps on them...  
-
+# ----------- This app gets a few months worth of minute data for each coin, and identifies if there was any pumps on them...  
 def getBinanceSymbols(symbol = 'BTC'):
+    
     # ---------- Finding all market pairs from Binance, REGEXing them, and placing them into a list:
     symbols = []
     exchange_info = client.get_exchange_info()
@@ -23,11 +24,11 @@ def getBinanceSymbols(symbol = 'BTC'):
     symbols = sorted(symbols) #Done for convenience...
     return(symbols)
 
-
 # ---------- Used to get a snapshot of the symbols data in 1 min time interval
 def getSymbolsData(symbol):
 
-    klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, "2 month ago UTC") # First ever pump on binance was on 06/09/2018 
+    #
+    klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_15MINUTE, "6 year ago UTC") # First ever pump on binance was on 06/09/2018 
     df = pd.DataFrame(klines, columns = ['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'])
     df = df.rename(columns={'Open time': 'Open_Time', "Close": "Close_Price", "Close Time": "Close_Time", "Quote asset volume": "BTC_Volume", "Number of trades": "Trades", 'Volume': 'Asset_Volume'}) # Renaming columns...
     df.insert(0, column='Symbol', value=symbol)   # Adding symbol name to columns...
@@ -52,29 +53,49 @@ def getSymbolsData(symbol):
 def addMovingAvCheckThresh(df, window):
     
     #Adding the MA column with our needed window 
-    df['priceMA_' + str(window)] = df['Close_Price'].rolling(window).mean()
     df['volumeMA_' + str(window)] = df['BTC_Volume'].rolling(window).mean()
 
     #Adding our threshold, adding our check that the time is less than 5 mins after the hour.  
     thresholdVolume = 150
-    thresholdPrice = 5
-    thresholdTime = 5 #Require minimum 5 mins after hour.
+    thresholdPercAboveOpen = 10
+    thresholdTime = 10 #Require minimum 5 mins after hour.
+    thresholdPecCloseBelowHigh = 20 # require 50% lower than high
     timeAfterHour = df['Open_Time'].dt.minute
-    percAbovePrice = 100 * (df['Close_Price'] - df['priceMA_' + str(window)]) / df['priceMA_' + str(window)]
+    percAboveOpen = 100 * (df['High'] - df['Open']) / df['Open']
+    percentCloseBelowHigh = - 100 * (df['Close_Price'] - df['High']) / df['High']
     percAboveVol = 100 * (df['BTC_Volume'] - df['volumeMA_' + str(window)]) / df['volumeMA_' + str(window)]
-    df['hasPumped'] = np.where((percAbovePrice >= thresholdPrice) & (percAboveVol >= thresholdVolume) & (timeAfterHour <= thresholdTime), 1, 0)
+    df['hasPumped'] = np.where(
+       (percAboveOpen >= thresholdPercAboveOpen) & 
+       (percAboveVol >= thresholdVolume) & 
+       (timeAfterHour <= thresholdTime) &
+       (percentCloseBelowHigh >= thresholdPecCloseBelowHigh)
+       , 1, 0)
+    df = df.drop(columns=['volumeMA_' + str(window)])
     return df
 
-# Getting 2 months of minute data for all symbols. 
-dfMain = pd.DataFrame()
-count = 0
-symbols = getBinanceSymbols()
-for i in symbols:
-    count+=1
-    print('=============== ' + str(count) + '/' + str(len(symbols)) + '================')
-    df = addMovingAvCheckThresh(getSymbolsData(i), 5) #5 min MA
-    df = df.loc[df['hasPumped'] == 1]
-    dfMain = pd.concat([dfMain, df])
-    print(dfMain)
-dfMain.to_csv('PumpData.csv')
+# ----------- Getting 2 months of minute data for all symbols. 
+import random
+def checkMAThresh(i):
+   time.sleep(random.random() * 10 * 60)
+   df = addMovingAvCheckThresh(getSymbolsData(i), 12)
+   df = df.loc[df['hasPumped'] == 1]
+   df = df.drop(columns=['hasPumped'])
+   print('----------------- ' + i + '  ----------------------------------')
+   print(df)
+   return df
 
+from concurrent.futures import ThreadPoolExecutor
+import time
+if __name__ == "__main__":
+  dfMain = pd.DataFrame()
+  symbols = getBinanceSymbols()
+  print(len(symbols))
+  executor = ThreadPoolExecutor(12)
+  futures = []
+  for symbol in symbols:
+     future = executor.submit(checkMAThresh, (symbol))
+     futures.append(future)
+  for future in futures:
+     dfMain = pd.concat([dfMain, future.result()])
+  print(dfMain)
+  dfMain.to_csv('PumpData.csv')
